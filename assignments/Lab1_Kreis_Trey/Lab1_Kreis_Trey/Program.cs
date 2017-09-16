ba435e9bceb854bda3edd86a09083e73d2cd5c27
Trey Kreis <trey.kreis@my.tccd.edu>
2017-09-16 10:30:01 -0500

    upload

---
+++ assignments/Lab1_Kreis_Trey/Lab1_Kreis_Trey/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Trey Kreis
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_Kreis_Trey {
+
+    class Program {
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
